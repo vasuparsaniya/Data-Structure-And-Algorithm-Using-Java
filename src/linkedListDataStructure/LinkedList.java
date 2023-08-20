455de236609838df2d92ce896d24ba00b80e9604
vasu <vasu@gmail.com>
2023-08-20 15:02:34 +0530

    structure

--- src/linkedListDataStructure/LinkedList.java
+++ src/linkedListDataStructure/LinkedList.java
@@ -1,6 +1,36 @@
 package linkedListDataStructure;
 
 public class LinkedList<T> {
+	
+	//Creating a Linked List Structure
 	Node head;
 	
+	class Node{
+		T data;
+		Node next;
+		
+		//constructor
+		Node(){
+			data=null;
+			next=null;
+		}
+		
+		Node(T d){
+			data=d;
+			next=null;
+		}
+	}
+	
+	//constructor
+	LinkedList(){
+		head = new Node();
+	}
+	
+	//Method to maintain the collection to be defined
+}
+
+class JLinkedList{
+	public static void main(String[] args) {
+		
+	}
 }
--- src/linkedListDataStructure/singleLinkedList/insertionOperation/SingleLinkedListInsertion.java
+++ src/linkedListDataStructure/singleLinkedList/insertionOperation/SingleLinkedListInsertion.java
@@ -1,11 +1,30 @@
 package linkedListDataStructure.singleLinkedList.insertionOperation;
 
 public class SingleLinkedListInsertion<T> {
+	
+	//Creating a Linked List Structure
 	Node head;
 	
 	class Node{
+		T data;
+		Node next;
 		
+		//constructor
+		Node(){
+			data=null;
+			next=null;
+		}
+		
+		Node(T d){
+			data=d;
+			next=null;
+		}
 	}
+	
+	SingleLinkedListInsertion(){
+		head = new Node();
+	}
+
 	public static void main(String[] args) {
 		
 	}
