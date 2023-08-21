7207fa5fcb0323bf5f5b9bb056eaa7d63b9d7970
vasu <vasu@gmail.com>
2023-08-21 10:18:47 +0530

    deletion at after key

--- src/linkedListDataStructure/singleLinkedList/deletionOperation/SingleLinkedListDeletion.java
+++ src/linkedListDataStructure/singleLinkedList/deletionOperation/SingleLinkedListDeletion.java
@@ -72,28 +72,68 @@ public class SingleLinkedListDeletion<T> extends SingleLinkedListInsertion<T>{
 		return x;
 	}
 	
+	//---------------Deletion At Any(Key is Given)-------------
+	//Case:-1 If key is not present then nothing will be deleted
+	//Case:-2 If key is present then delete After that key
+	//Case:-3 If key is present then delete Before that key
+	
+	public void deleteAfterKey(T key) {
+		Node currNode=this.head.next;
+		Node temp=currNode.next;
+		
+		if(this.head.next == null) {
+			System.out.println("List is Empty!! So deletion after key is failed..");
+		}else {
+			while(currNode != null) {
+				if(currNode.data == key) {
+					if(temp == null) {
+						System.out.println("Key found at the end of the list.So deletion after that key is not posible");
+						break;
+					}else {
+						currNode.next=temp.next;
+						temp.next=null;
+						System.out.println("Element is deleted successfully after key "+key+" "+"and deleted element is "+" "+currNode.data);
+						break;
+					}
+				}else {
+					if(temp == null) {
+						System.out.println("Key is not found");
+						break;
+					}
+					temp=temp.next;
+					currNode=currNode.next;
+				}
+				
+			}
+		}
+	}
+	
 	
 	//----------------------------------
 	public static void main(String[] args) {
 
 		SingleLinkedListDeletion<Integer> list=new SingleLinkedListDeletion<>();
-		
+//		
 		list.insertFront(2);
 		list.insertFront(4);
+
 		list.printLinkedList();
 		
 		list.insertEnd(6);
 		list.printLinkedList();
 
-		list.deletionFront();
-		list.printLinkedList();
-		list.deletionFront();
-		list.printLinkedList();
-		list.deletionFront();
-		list.printLinkedList();
-		list.deletionFront();
-		list.printLinkedList();
-		list.deletionEnd();
+//		list.deletionFront();
+//		list.printLinkedList();
+//		list.deletionFront();
+//		list.printLinkedList();
+//		list.deletionFront();
+//		list.printLinkedList();
+//		list.deletionFront();
+//		list.printLinkedList();
+//		list.deletionEnd();
+//		list.printLinkedList();
+		
+		list.deleteAfterKey(6);
 		list.printLinkedList();
 	}
 }
